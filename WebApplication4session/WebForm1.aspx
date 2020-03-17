1ca91d4e19638ac2431d810fdd13c4555a084fbc
nallalaharishmareddy@gmail.com <nallalaharishmareddy@gmail.com>
2020-03-17 16:43:21 +0530

    Add project files.

---
+++ WebApplication4session/WebForm1.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication4session.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+       
+        <div>
+             <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+             session variable<br />
+             <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+             application variable</div>
+        <p>
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ WebApplication4session/WebForm2.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace WebApplication4session
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        SqlConnection con = null;
+        SqlDataAdapter adp = null;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con = new SqlConnection(ConfigurationManager.ConnectionStrings["SqlCon"].ToString());
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            adp = new SqlDataAdapter("sp_validateLogin", con);
+            adp.SelectCommand.CommandType = CommandType.StoredProcedure;
+            adp.SelectCommand.Parameters.AddWithValue("@U", txtusername.Text);
+            adp.SelectCommand.Parameters.AddWithValue("@P", txtpassword.Text);
+            SqlParameter p = new SqlParameter("@C", SqlDbType.Int);
+            p.Direction = ParameterDirection.Output;
+            adp.SelectCommand.Parameters.Add(p);
+            DataSet ds = new DataSet();
+            adp.Fill(ds, "L");
+        }
+    }
+}
